d6737cc5a13971f88013ac1bcabb0c627d08192b
stognienkovladislav <stognienkovv@gmail.com>
2017-11-26 20:28:03 +0200

    chat client/serv

---
+++ Echo/client.py
@@ -0,0 +1,13 @@
+#echo_client
+
+import socket
+
+host = socket.gethostname()
+port = 12345
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+s.sendall(b'Hello World')
+data = s.recv(1024)
+s.close()
+print("Received", repr(data))
+
